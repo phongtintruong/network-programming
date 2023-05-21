bf2bae49a2cad231f375db98c62854b82f39a930
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2023-05-22 00:04:05 +0700

    Update chat_server_poll

--- homework/homework3/chat_server_poll.c
+++ homework/homework3/chat_server_poll.c
@@ -8,6 +8,18 @@
 
 #include <string.h>
 #include <poll.h>
+#include <time.h>
+
+void get_time_to_buf(char* buf){
+    // Get the current time
+    time_t current_time;
+    time(&current_time);
+
+    // Format the time as "YYYY/MM/DD HH:MM:SSAM/PM"
+    struct tm* time_info;
+    time_info = localtime(&current_time);
+    strftime(buf, 24, "[%Y/%m/%d %H:%M:%S%p]", time_info);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -42,126 +54,137 @@ int main(int argc, char *argv[]) {
 
     printf("Server started. Waiting for connections...\n");
 
-    struct pollfd fds[max_noclient];
-
-    fd_set fdread;
-    int clients[max_noclient];
-    char *client_id[max_noclient];
-    int client_state[max_noclient];
-    for (int i=0;i<max_noclient;i++){
-        client_state[i]=1;
+    struct pollfd fds[max_noclient+1];
+    char *client_id[max_noclient+1];
+    int client_state[max_noclient+1];
+    for (int i=1;i<max_noclient+1;i++){
+        client_state[i]=0; //Haven't login yet
     }
-    int num_clients=0;
+    int nfds = 1;
+
+    fds[0].fd = listener;
+    fds[0].events = POLLIN;
+
     char buf[256];
-    struct timeval tv;
 
     while(1){
-        FD_ZERO(&fdread);
-        FD_SET(listener, &fdread);
-
-        int maxdp = listener;
-        for (int i=0;i<num_clients;i++){
-            FD_SET(clients[i],&fdread);
-            if (clients[i]>maxdp){
-                maxdp=clients[i];
-            }
+        
+        int ret = poll(fds, nfds, 60000);
+        if (ret < 0){
+            perror("poll() failed");
+            break;
         }
 
-        tv.tv_sec=60;
-        tv.tv_usec=0;
-
-        int ret = select(maxdp+1, &fdread, NULL, NULL, &tv);
-        if (ret<0){
-            perror("select() failed");
-            return 1;
-        }
-
-        if (ret==0){
+        if (ret == 0){
             printf("TIME OUT!!!\n");
             continue;
         }
 
-        if (FD_ISSET(listener, &fdread)){
+        if (fds[0].revents & POLLIN){
             int client = accept(listener, NULL, NULL);
             if (client==-1){
                 perror("accept() failed");
                 // continue;
             } else {
-                if (num_clients==max_noclient){
+                if (nfds>max_noclient){
                     int check = 0;
-                    for (int i=0;i<max_noclient;i++){
-                        if (client_state[i]==0){
-                            check++;
-                            printf("New user connected: %d\n", client);
-                            clients[i] = client;
-                            client_state[i]=1;
-                            break;                            
-                        }
-                    }
+                    // for (int i=0;i<max_noclient;i++){
+                    //     if (client_state[i]==0){
+                    //         check++;
+                    //         printf("New user connected: %d\n", client);
+                    //         clients[i] = client;
+                    //         client_state[i]=1;
+                    //         break;                            
+                    //     }
+                    // }
                     if (check==0){
-                        char *msg = "number of clients limit exceeds, comeback later.\n";
+                        char msg[256];
+                        get_time_to_buf(msg);
+                        strcat(msg, " number of clients limit exceeds, comeback later.\n");
                         send(client, msg, strlen(msg), 0);
                         printf("number of clients limit exceeds, excess client will not be processed\n");
                         close(client);
                     }
                 } else {
                     printf("New user connected: %d\n", client);
-                    clients[num_clients]=client;
-                    client_state[num_clients]=1;
-                    num_clients++;
+                    fds[nfds].fd=client;
+                    fds[nfds].events = POLLIN;
+                    nfds++;
                 }
             }
         }
 
-        for (int i=0;i<num_clients;i++){
-            if (FD_ISSET(clients[i], &fdread)){
-                ret = recv(clients[i], buf, sizeof(buf), 0);
+
+        for (int i=1;i<nfds;i++){
+            if (fds[i].revents & POLLIN){
+                ret = recv(fds[i].fd, buf, sizeof(buf), 0);
                 if (ret <= -1){
                     perror("recv() failed");
-                    FD_CLR(clients[i], &fdread);
-                    close(clients[i]);
-                    client_state[i]=0;
-                    clients[i]=-1;
-                    continue;
+                    close(fds[i].fd);
+                    if (i<nfds-1){
+                        free(client_id[i]);
+                        fds[i] = fds[nfds-1];
+                        client_id[i]=malloc(strlen(client_id[nfds-1])+1);
+                        strcpy(client_id[i], client_id[nfds-1]);
+                    }
+                    free(client_id[nfds-1]);
+                    client_state[nfds-1]=0;
+                    nfds--;
+                    i--;
                 } else if (ret==0){
                     perror("client disconnected");
-                    FD_CLR(clients[i], &fdread);
-                    close(clients[i]);
-                    client_state[i]=0;
-                    clients[i]=-1;
-                    continue;
+                    close(fds[i].fd);
+                    if (i<nfds-1){
+                        free(client_id[i]);
+                        fds[i] = fds[nfds-1];
+                        client_id[i]=malloc(strlen(client_id[nfds-1])+1);
+                        strcpy(client_id[i], client_id[nfds-1]);
+                    }
+                    free(client_id[nfds-1]);
+                    client_state[nfds-1]=0;
+                    nfds--;
+                    i--;
                 } else {
                     buf[ret] = 0;
-                    printf("Received from %d: %s\n", clients[i], buf);
-                    if (client_state[i]==1){
+                    printf("Received from %d: %s\n", fds[i].fd, buf);
+                    
+                    if (client_state[i]==0){
                         char cmd[32], id[32], tmp[32];
                         ret = sscanf(buf, "%s%s%s", cmd, id, tmp);
                         if (ret==2){
                             if (strcmp(cmd, "client_id:")==0){
-                                char *msg = "You are signed in. Enter message.\n";
-                                send(clients[i], msg, strlen(msg), 0);
+                                char msg[256];
+                                get_time_to_buf(msg);
+                                strcat(msg, " You are signed in. Enter message.\n");
+                                send(fds[i].fd, msg, strlen(msg), 0);
 
                                 client_id[i]=malloc(strlen(id)+1);
                                 strcpy(client_id[i], id);
-                                client_state[i]=2;
+                                client_state[i]=1;
                             } else {
-                                char *msg = "Wrong usage. Usage: client_id: <id>.\n";
-                                send(clients[i], msg, strlen(msg), 0);
+                                char msg[256];
+                                get_time_to_buf(msg);
+                                strcat(msg, " Wrong usage. Usage: client_id: <id>.\n");
+                                send(fds[i].fd, msg, strlen(msg), 0);
                             }
                         } else {
-                            char *msg = "Wrong usage. Usage: client_id: <id>.\n";
-                            send(clients[i], msg, strlen(msg), 0);
+                            char msg[256];
+                            get_time_to_buf(msg);
+                            strcat(msg, " Wrong usage. Usage: client_id: <id>.\n");
+                            send(fds[i].fd, msg, strlen(msg), 0);
                         }
                     } else {
                         char sendbuf[256];
 
-                        strcpy(sendbuf, client_id[i]);
+                        get_time_to_buf(sendbuf);
+                        strcat(sendbuf, " ");
+                        strcat(sendbuf, client_id[i]);
                         strcat(sendbuf, ": ");
                         strcat(sendbuf, buf);
 
-                        for (int j=0;j<num_clients;j++){
-                            if (client_state[j]==2&&j!=i){
-                                send(clients[j], sendbuf, strlen(sendbuf), 0);
+                        for (int j=1;j<nfds;j++){
+                            if (client_state[j]==1&&j!=i){
+                                send(fds[j].fd, sendbuf, strlen(sendbuf), 0);
                             }
                         }
                     }
