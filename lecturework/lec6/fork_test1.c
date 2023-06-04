fa567db56097a5adfa1793c19c0d2ff247960768
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2023-06-04 15:51:12 +0700

    New lecturework lec6

---
+++ lecturework/lec6/fork_test1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Before fork()\n");
+    int cid = fork();
+    int pid = getpid();
+    if (cid<0){
+        perror("fork() failed");
+        exit(1);
+    } else if (cid==0){
+        printf("Start\n");
+        sleep(20);
+        printf("Child => cid: %d and pid: %d\n", cid, pid);
+        exit(0);
+    } else {
+        printf("Parent => cid: %d and pid: %d\n", cid, pid);  
+    }
+    return 0;
+}
