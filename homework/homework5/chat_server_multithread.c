1e15a6e7ea299f8b2271619ad4f972f934f816da
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2023-06-18 23:47:42 +0700

    Update homework5 chat_server_multithread and lec7 thread_test

--- homework/homework5/chat_server_multithread.c
+++ homework/homework5/chat_server_multithread.c
@@ -10,6 +10,9 @@
 #include <time.h>
 #include <pthread.h>
 
+int client_count=0;
+pthread_mutex_t client_count_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void get_time_to_buf(char* buf){
     // Get the current time
     time_t current_time;
@@ -20,6 +23,8 @@ void get_time_to_buf(char* buf){
     time_info = localtime(&current_time);
     strftime(buf, 24, "[%Y/%m/%d %H:%M:%S%p]", time_info);
 }
+
+void *client_thread(void *arg);
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -54,4 +59,53 @@ int main(int argc, char *argv[]) {
 
     printf("Server started. Waiting for connections...\n");
 
-PTHREAD_MUTEX_INITIALIZER
+
+    while (1){
+        int client = accept(listener, NULL, NULL);
+        if (client==-1){
+            perror("accept() failed");
+        } else {
+            if (client_count>=max_noclient){
+                char msg[256];
+                get_time_to_buf(msg);
+                strcat(msg, " number of clients limit exceeds, comeback later.\n");
+                send(client, msg, strlen(msg), 0);
+                printf("number of clients limit exceeds, excess client will not be processed\n");
+                close(client);
+            } else {
+                void *ret;
+                printf("New user connected: %d\n", client);
+                pthread_t thread_id;
+                client_count++;
+                if(pthread_create(&thread_id, NULL, client_thread, (void *)&client)!=0){
+                    perror("pthread_create() failed");
+                    return 1;
+                }
+                if (pthread_join(thread_id, &ret)!=0){
+                    perror("pthread_create() failed");
+                    return 1;
+                }
+                pthread_detach(thread_id);
+                printf("Thread exited with '%s'\n", (char *)ret);
+            }
+        }
+    }
+    close(listener);
+    return 0;    
+}
+
+void *client_thread(void *param){
+    char *client_thread_ret;
+    int client = *(int *)param;
+    char buf[256];
+
+    while (1){
+        int ret = recv(client, buf, sizeof(buf), 0);
+        if (ret<=-1){
+            perror("recv() failed");
+            close(client);
+            strcpy(client_thread_ret, "recv() failed");
+            pthread_exit(client_thread_ret);
+        }
+    }
+}
--- lecturework/lec7/thread_test.c
+++ lecturework/lec7/thread_test.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <unistd.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
 #include <pthread.h>
-
-static long num_steps = 100000;
-
 void *thread_proc(void *);
-
-int main(){
+int main() { 
     pthread_t thread_id;
-    if (pthread_create(&thread_id, NULL, thread_proc, NULL)){
+    if (pthread_create(&thread_id, NULL, thread_proc, NULL)) {
         printf("Failed to create new thread\n");
         return 1;
     }
     pthread_join(thread_id, NULL);
-    int i;
-    double x, sum =0.0;
-
-    double step = 1.0 / (double)num_steps;
-    for (i = 0; i < num_steps; i++){
-        x = (i+0.5)*step;
-        sum += 4.0/(1.0+x*x);
-    }
-
-    double pi = step * sum;
-    printf ("PI=%.10f\n", pi);
+    return 0; 
 }
+void *thread_proc(void *arg) {
+    printf("start\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Child thread %d\n", i);
+        sleep(1);
+    }
+}
+
