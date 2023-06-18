d6e71bb8b59242f578ebc6770d17e66676a3ee83
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2023-06-19 03:11:03 +0700

    Update homework5 chat_server_multithread: final version

--- homework/homework5/chat_server_multithread.c
+++ homework/homework5/chat_server_multithread.c
@@ -10,8 +10,10 @@
 #include <time.h>
 #include <pthread.h>
 
+int client_id[100][3];
+char *client_name[100];
 int client_count=0;
-pthread_mutex_t client_count_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void get_time_to_buf(char* buf){
     // Get the current time
@@ -57,6 +59,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    for (int i=0;i<100;i++){
+        client_id[i][0] = -1;
+        client_id[i][1] = -1; // -1 means not connected
+    }
+
     printf("Server started. Waiting for connections...\n");
 
 
@@ -65,28 +72,58 @@ int main(int argc, char *argv[]) {
         if (client==-1){
             perror("accept() failed");
         } else {
-            if (client_count>=max_noclient){
+            if (client_count>max_noclient){
                 char msg[256];
                 get_time_to_buf(msg);
                 strcat(msg, " number of clients limit exceeds, comeback later.\n");
                 send(client, msg, strlen(msg), 0);
                 printf("number of clients limit exceeds, excess client will not be processed\n");
                 close(client);
+            } else if (client_count==max_noclient){
+                int check=0;
+                for (int i=0;i<client_count;i++){
+                    if (client_id[i][1]==-1){
+                        check=1;
+                        printf("New user connected: %d\n", client);
+                        pthread_t thread_id;
+                        pthread_mutex_lock(&mutex);
+                        client_id[i][0] = client;
+                        client_id[i][1] = 0; // 0 means not login
+                        pthread_mutex_unlock(&mutex);
+                        printf("start thread %d\n", client);
+                        if(pthread_create(&thread_id, NULL, client_thread, (void *)&i)!=0){
+                            perror("pthread_create() failed");
+                            return 1;
+                        }
+                        
+                        pthread_detach(thread_id);
+                        break;
+                    }
+                }
+                if (check==0){
+                    char msg[256];
+                    get_time_to_buf(msg);
+                    strcat(msg, " number of clients limit exceeds, comeback later.\n");
+                    send(client, msg, strlen(msg), 0);
+                    printf("number of clients limit exceeds, excess client will not be processed\n");
+                    close(client);
+                }            
             } else {
-                void *ret;
                 printf("New user connected: %d\n", client);
                 pthread_t thread_id;
+                pthread_mutex_lock(&mutex);
                 client_count++;
-                if(pthread_create(&thread_id, NULL, client_thread, (void *)&client)!=0){
+                client_id[client_count-1][0] = client;
+                client_id[client_count-1][1] = 0; // 0 means not login
+                pthread_mutex_unlock(&mutex);
+                int client_index = client_count-1;
+                printf("start thread %d\n", client);
+                if(pthread_create(&thread_id, NULL, client_thread, (void *)&client_index)!=0){
                     perror("pthread_create() failed");
                     return 1;
                 }
-                if (pthread_join(thread_id, &ret)!=0){
-                    perror("pthread_create() failed");
-                    return 1;
-                }
+                
                 pthread_detach(thread_id);
-                printf("Thread exited with '%s'\n", (char *)ret);
             }
         }
     }
@@ -95,8 +132,8 @@ int main(int argc, char *argv[]) {
 }
 
 void *client_thread(void *param){
-    char *client_thread_ret;
-    int client = *(int *)param;
+    int client_index = *(int *)param;
+    int client = client_id[client_index][0];
     char buf[256];
 
     while (1){
@@ -104,8 +141,68 @@ void *client_thread(void *param){
         if (ret<=-1){
             perror("recv() failed");
             close(client);
-            strcpy(client_thread_ret, "recv() failed");
-            pthread_exit(client_thread_ret);
+
+            pthread_mutex_lock(&mutex);
+            client_id[client_index][0] = -1;
+            client_id[client_index][1] = -1;
+            free(client_name[client_index]);
+            pthread_mutex_unlock(&mutex);
+            break;
+        } else if (ret==0){
+            perror("client disconnected");
+            close(client);
+
+            pthread_mutex_lock(&mutex);
+            client_id[client_index][0] = -1;
+            client_id[client_index][1] = -1;
+            free(client_name[client_index]);
+            pthread_mutex_unlock(&mutex);
+            printf("test disconnect\n");
+            break;
+        } else {
+            buf[ret] = 0;
+            printf("Received from %d: %s\n", client, buf);
+            if (client_id[client_index][1]==0){
+                char cmd[32], id[32], tmp[32];
+                ret = sscanf(buf, "%s %s %s", cmd, id, tmp);
+                if (ret==2){
+                    if (strcmp(cmd, "client_id:")==0){
+                        char msg[256];
+                        get_time_to_buf(msg);
+                        strcat(msg, " You are signed in. Enter message.\n");
+                        send(client, msg, strlen(msg), 0);
+
+                        client_id[client_index][1] = 1; // 1 means login
+                        client_name[client_index] = malloc(strlen(id)+1);
+                        strcpy(client_name[client_index], id);
+                    } else {
+                        char msg[256];
+                        get_time_to_buf(msg);
+                        strcat(msg, " Wrong usage. Usage: client_id: <id>.\n");
+                        send(client, msg, strlen(msg), 0);
+                    }
+                } else {
+                    char msg[256];
+                    get_time_to_buf(msg);
+                    strcat(msg, " Wrong usage. Usage: client_id: <id>.\n");
+                    send(client, msg, strlen(msg), 0);
+                }
+            } else {
+                char sendbuf[256];
+
+                get_time_to_buf(sendbuf);
+                strcat(sendbuf, " ");
+                strcat(sendbuf, client_name[client_index]);
+                strcat(sendbuf, ": ");
+                strcat(sendbuf, buf);
+
+                for (int j=0;j<client_count;j++){
+                    if (client_id[j][0]!=client_id[client_index][0]&&client_id[j][1]==1){
+                        send(client_id[j][0], sendbuf, strlen(sendbuf), 0);
+                    }
+                }
+            }
         }
     }
+    close(client);
 }
